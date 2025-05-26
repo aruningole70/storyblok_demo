07579ff65a87743f94b2238aa8a2f1c63af99ba8
aruningole70 <aruningole70@gmail.com>
2025-05-26 12:41:23 +0530

    added shema and story schema to bolck and story creation

--- src/Pages/Pages.tsx
+++ src/Pages/Pages.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
-import { SbBlokData, StoryblokComponent, storyblokEditable } from '@storyblok/react'
-import axios from 'axios';
-import { useStoryblokApi } from '@storyblok/react';
+import React, { useState } from "react";
+import {
+  SbBlokData,
+  StoryblokComponent,
+  storyblokEditable,
+} from "@storyblok/react";
+import axios from "axios";
 
 interface PageProps {
   blok: SbBlokData & {
@@ -11,64 +14,94 @@ interface PageProps {
 
 const Pages = ({ blok }: PageProps) => {
   console.log("pages blok =====", blok);
-  const storyblokApi = useStoryblokApi();
-  const header = {
-    name: "text4",
-    is_nestable: true,
+  const blockhHeader = {
+    name: "new_test_3",
+    display_name: null,
+    schema: {
+      headline: {
+        type: "textarea",
+        pos: 0,
+        translatable: true,
+        description: "This field is used to render an H1 title",
+      },
+      firstName: {
+        type: "text",
+        pos: 0,
+        translatable: true,
+        description: "This field is used to render an H2 title",
+      },
+      lastName: {
+        type: "text",
+        pos: 0,
+        translatable: true,
+        description: "This field is used to render an H3 title",
+      },
+      salary: {
+        type: "number",
+        pos: 0,
+        translatable: true,
+        description: "This field is used to render an H3 title",
+      },
+    },
     is_root: false,
-    internal_tag_ids: [],
-    description: ""
-  }
-  const storyheaders ={
-    name: "demo2",
-    slug: "demo2",
+    is_nestable: true,
+  };
+  const storyheaders = {
+    name: "new_test_story_3",
+    slug: "new_test_story_3",
     parent_id: null,
     content: {
-        component: "page"
+      component: blockhHeader.name,
     },
     user_ids: [],
     space_role_ids: [],
     is_startpage: false,
     preset_id: null,
-    translated_slugs_attributes: []
-}
+    translated_slugs_attributes: [],
+  };
   const managementApi = axios.create({
-    baseURL: 'https://app.storyblok.com/v1',
+    baseURL: "https://app.storyblok.com/v1",
     headers: {
-      Authorization: 'dVkWGfTr2LUoEQvimi7EHQtt-291155--aWMXgdEPN-BRm3Rzkkn',
+      Authorization: "dVkWGfTr2LUoEQvimi7EHQtt-291155--aWMXgdEPN-BRm3Rzkkn",
     },
   });
   const createBlock = async () => {
-    try {
-      const response: any = await managementApi.post("/spaces/340036/components", {
-        "component": header
+    managementApi
+      .post("/spaces/340036/components", {
+        component: blockhHeader,
       })
-      console.log(response)
-    } catch (err) {
-      console.log(err);
-
-    }
-  }
-  const createStory =async () => {
-    try {
-      const response: any = await managementApi.post("/spaces/340036/stories", {...storyheaders})
-      console.log(response)
-    } catch (err) {
-      console.log(err);
-
-    }
-  }
+      .then((response: any) => {
+        console.log("block res ======", response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const createStory = async () => {
+    managementApi
+      .post("/spaces/340036/stories", {
+        ...storyheaders,
+      })
+      .then((response: any) => {
+        console.log("block creation res====", response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <>
       <main {...storyblokEditable(blok)}>
-        {blok && blok.body && blok.body.map((nestedBlok) => (
-          <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
-        ))}
+        {blok &&
+          blok.body &&
+          blok.body.map((nestedBlok) => (
+            <StoryblokComponent blok={nestedBlok} key={nestedBlok._uid} />
+          ))}
       </main>
       <button onClick={createBlock}>Creat block</button>
       <button onClick={createStory}>Creat story</button>
     </>
-  )
-}
+  );
+};
 
-export default Pages
+export default Pages;
